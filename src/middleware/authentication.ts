d44600d44b778c9b69fe88cdf7d7d081ed8a3d99
Dinesh10731j <www.dineshtmg1122@gmail.com>
2026-03-18 11:10:50 +0545

    minor changes done

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { UserPayload } from "../dto/interface";
 import { envConfig } from "../configs/env.config";
